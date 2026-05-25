ccc84b6f942b57d23ff5b7302ddeeccedaed185d
Cal <168704009+nfworking@users.noreply.github.com>
2026-05-25 20:06:38 +1000

    init

---
+++ docker.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2026 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.docker.com/
+
+APP="Docker"
+var_tags="${var_tags:-docker}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-4}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_arm64="${var_arm64:-no}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  msg_info "Updating base system"
+  $STD apt update
+  $STD apt upgrade -y 
+  msg_ok "Base system updated"
+
+  msg_info "Updating Docker Engine"
+  $STD apt install --only-upgrade -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-buildx-plugin
+  msg_ok "Docker Engine updated"
+
+  if docker ps -a --format '{{.Image}}' | grep -q '^portainer/portainer-ce:latest$'; then
+    msg_info "Updating Portainer"
+    $STD docker pull portainer/portainer-ce:latest
+    $STD docker stop portainer
+    $STD docker rm portainer
+    $STD docker volume create portainer_data >/dev/null 2>&1
+    $STD docker run -d \
+      -p 8000:8000 \
+      -p 9443:9443 \
+      --name=portainer \
+      --restart=always \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v portainer_data:/data \
+      portainer/portainer-ce:latest
+    msg_ok "Updated Portainer"
+  fi
+
+  if docker ps -a --format '{{.Names}}' | grep -q '^portainer_agent$'; then
+    msg_info "Updating Portainer Agent"
+    $STD docker pull portainer/agent:latest
+    $STD docker stop portainer_agent
+    $STD docker rm portainer_agent
+    $STD docker run -d \
+      -p 9001:9001 \
+      --name=portainer_agent \
+      --restart=always \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+      portainer/agent
+    msg_ok "Updated Portainer Agent"
+  fi
+  msg_ok "Updated successfully!"
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} If you installed Portainer, access it at the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}https://${IP}:9443${CL}"
